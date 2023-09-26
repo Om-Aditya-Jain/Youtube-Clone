2fbf80eb9b59e7334928d22f71591258fec18880
Om-Aditya-Jain <0808io201033.ies@ipsacademy.org>
2023-09-26 12:36:49 +0530

    added Header Component

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header";
+export { default as Feed } from "./Feed";
+export { default as SearchResult } from "./SearchResult";
+export { default as VideoDetails } from "./VideoDetails";
--- src/context/contextApi.jsx
+++ src/context/contextApi.jsx
@@ -4,35 +4,37 @@ import { fetchDataFromApi } from "../utils/api";
 export const Context = createContext();
 
 export const AppContext = (props) => {
-  const [loading, setLoading] = useState(false);
-  const [searchResults, setSearchResults] = useState(false);
-  const [mobileMenu, setMobileMenu] = useState(false);
-  const [selectCategories, setSelectCategories] = useState("New");
+	const [loading, setLoading] = useState(false);
+	const [searchResults, setSearchResults] = useState(false);
+	const [mobileMenu, setMobileMenu] = useState(false);
+	const [selectCategories, setSelectCategories] = useState("New");
 
-  useEffect(() => {
-    fetchSelectedCategoryData(selectCategories);
-  }, [selectCategories]);
+	useEffect(() => {
+		fetchSelectedCategoryData(selectCategories);
+	}, [selectCategories]);
 
-  const fetchSelectedCategoryData = (query) => {
-    setLoading(true);
-    fetchDataFromApi(`search/?q=${query}`).then((res) => console.log(res));
-    setLoading(false);
-  };
+	const fetchSelectedCategoryData = (query) => {
+		setLoading(true);
+		fetchDataFromApi(`search/?q=${query}`).then(({ contents }) =>
+			setSearchResults(contents)
+		);
+		setLoading(false);
+	};
 
-  return (
-    <Context.Provider
-      value={{
-        loading,
-        setLoading,
-        searchResults,
-        setSearchResults,
-        mobileMenu,
-        setMobileMenu,
-        selectCategories,
-        setSelectCategories,
-      }}
-    >
-      {props.children}
-    </Context.Provider>
-  );
+	return (
+		<Context.Provider
+			value={{
+				loading,
+				setLoading,
+				searchResults,
+				setSearchResults,
+				mobileMenu,
+				setMobileMenu,
+				selectCategories,
+				setSelectCategories,
+			}}
+		>
+			{props.children}
+		</Context.Provider>
+	);
 };
