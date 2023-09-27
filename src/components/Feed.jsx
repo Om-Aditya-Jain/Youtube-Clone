f218b2ee99ea47bb0691b521631ffe1d706b12dd
Om-Aditya-Jain <0808io201033.ies@ipsacademy.org>
2023-09-27 12:38:37 +0530

    Created Sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
 
 const Feed = () => {
-	return <div>Feed</div>;
+	const { loading, searchResults } = useContext(Context);
+
+	useEffect(() => {
+		document.getElementById("root").classList.remove("custom-h");
+	}, []);
+
+	return (
+		<div className="flex flex-row h-[calc(100%-56px)]">
+			<LeftNav />
+		</div>
+	);
 };
 
 export default Feed;
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 export { default as Header } from "./Header";
+export { default as LeftNav } from "./LeftNav";
+export { default as LeftNavMenuItem } from "./LeftNavMenuItem";
 export { default as Feed } from "./Feed";
 export { default as SearchResult } from "./SearchResult";
 export { default as VideoDetails } from "./VideoDetails";
